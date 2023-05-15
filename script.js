6206c04abf8ada9c306386a22bfd79f975bea4b5
Tyree <tyreewhite01102006@gmail.com>
2023-05-15 11:40:28 -0500

    Add event lstnrs to btns and #s

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let numbers = document.querySelectorAll('.number');
+numbers.forEach(number => number.addEventListener('click', printNumber));
 let decimal = document.querySelector('#decimalBtn');
-
+decimal.addEventListener('click', printDecimal);
+let screen = document.querySelector('.screen');
 
 function add(x, y) {
     return x + y;
@@ -16,4 +18,12 @@ function multiply(x, y) {
 
 function divide(x, y) {
     return x / y;
+}
+
+function printDecimal() {
+    return screen.textContent += this.value;
+}
+
+function printNumber() {
+    return screen.textContent += this.value
 }
